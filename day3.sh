cc6221ac8e40c764db09cc7dc5f1fda882bac521
Fred Hebert <mononcqc@ferd.ca>
2021-12-03 22:33:08 +0000

    shorten day3

--- day3.sh
+++ day3.sh
@@ -26,38 +26,32 @@ END {
 # array, then process them as the exercise demands.
 echo "part 2"
 awk '
-function copy(arr,out,    i,j) {
-    delete out
-    for (i in arr) { for (j in arr[i]) { out[i][j]=arr[i][j] } }
-}
 function common(k,arr,    i,c) {
-    for (i in arr) { c += arr[i][k]*2-1 }
-    return c>=0
+  for (i in arr) { c += arr[i][k]*2-1 }
+  return c>=0
 }
 function cull(k,v,arr,    i) {
-    for (i in arr) { if (arr[i][k] != v) { delete arr[i] } }
+  for (i in arr) { if (arr[i][k] != v) { delete arr[i] } }
 }
 function to_n(arr,  i,j,c) {
-    for (i in arr) { for (j in arr[i]) { c=lshift(c,1)+arr[i][j] } }
-    return c
+  for (i in arr) { for (j in arr[i]) { c=lshift(c,1)+arr[i][j] } }
+  return c
 }
 
 BEGIN {FS=""}
 
-{ for (i=1; i<=NF; i++) { d[NR][i]=$i } }
+{ for (i=1; i<=NF; i++) { o2[NR][i]=$i; co2[NR][i]=$i } }
 
 END {
-    copy(d, o2)
-    copy(d, co2)
-    for (i=1; i<=NF; i++) {
-        cull(i,common(i,o2),o2)
-        if (length(o2) == 1) { break; }
-    }
-    for (i=1; i<=NF; i++) {
-        cull(i,!(0+common(i,co2)),co2)
-        if (length(co2) == 1) { break; }
-    }
-    print to_n(o2)*to_n(co2)
+  for (i=1; i<=NF; i++) {
+    cull(i,common(i,o2),o2)
+    if (length(o2) == 1) { break }
+  }
+  for (i=1; i<=NF; i++) {
+    cull(i,!(0+common(i,co2)),co2)
+    if (length(co2) == 1) { break }
+  }
+  print to_n(o2)*to_n(co2)
 }
 ' day3.input
 
